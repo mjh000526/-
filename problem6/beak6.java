f85c54680a34a1ce93847ea3cb442d270c088496
mjh000526 <mjh000526@gmail.com>
2019-06-06 19:19:49 +0900

    problem6

---
+++ problem6/beak6.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class beak6{
+           public static void main(String[] args){
+            double a;
+            double b;
+            Scanner num=new Scanner(System.in);
+            a=num.nextInt();
+            b=num.nextInt();
+            System.out.println(a/b);
+           }
+}
