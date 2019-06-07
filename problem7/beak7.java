c372a27af64498eed219e3b17f65abbb1252fc5e
mjh000526 <mjh000526@gmail.com>
2019-06-07 14:01:03 +0900

    problem7

---
+++ problem7/beak7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class beak7{
+            public static void main(String[] args){
+            int a;
+            int b;
+            Scanner num=new Scanner(System.in);
+            a=num.nextInt();
+            b=num.nextInt();
+            System.out.println(a+b);
+            System.out.println(a-b);
+            System.out.println(a*b);
+            System.out.println(a/b);
+            System.out.println(a%b);
+            }
+}
