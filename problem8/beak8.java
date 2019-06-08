2aeb973ff36a8a2643c78749437ca6ef618bfe21
mjh000526 <mjh000526@gmail.com>
2019-06-08 23:55:50 +0900

    problem8

---
+++ problem8/beak8.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class beak8{
+         public static void main(String[] args){
+             System.out.println("강한친구 대한육군");
+             System.out.println("강한친구 대한육군");
+         }
+}
