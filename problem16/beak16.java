a4062c34e76143c27c9cb0b7b03586a389d0e1a4
mjh000526 <mjh000526@gmail.com>
2019-07-01 15:18:23 +0900

    problem16

---
+++ problem16/beak16.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class beak16{
+         public static void main(String[] args){
+                 Scanner scan=new Scanner(System.in);
+                 int a=scan.nextInt();
+                 if(a>=90)
+                 {
+                        System.out.println("A");
+                 }
+                 else if(a>=80)
+                 {
+                        System.out.println("B");
+                 }
+                 else if(a>=70)
+                 {
+                        System.out.println("C");
+                 }
+                 else if(a>=60)
+                 {
+                        System.out.println("D");
+                 }
+                 else
+                 {
+                        System.out.println("F");
+                 } 
+         }
+}
