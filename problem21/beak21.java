ce9883df3cbcdb994c1bacef167b1d27ff8b1124
mjh000526 <mjh000526@gmail.com>
2019-07-06 17:59:26 +0900

    problem21

---
+++ problem21/beak21.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class beak21{
+        public static void main(String[] args){
+                     Scanner scan=new Scanner(System.in);
+                     int a=scan.nextInt();
+                     for(int i=0;i<a;i++)
+                     {
+                              int one=scan.nextInt();
+                              int two=scan.nextInt();
+                              System.out.print((one+two)+"\n");
+                     }
+        }       
+}
