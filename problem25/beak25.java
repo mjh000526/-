616f7e75c23e0905a1e310e18e712ff712f33e6f
mjh000526 <mjh000526@gmail.com>
2019-07-08 23:48:06 +0900

    problem25

---
+++ problem25/beak25.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class beak25{
+        public static void main(String[] args){
+                 Scanner scan=new Scanner(System.in);
+                 int a=scan.nextInt();
+                 for(int i=a;i>0;i--)
+                 {
+                          System.out.print(i+"\n");
+                 } 
+        }
+}
