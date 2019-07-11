457033c8e1bff0263582b45411ff5a873194eb55
mjh000526 <mjh000526@gmail.com>
2019-07-11 14:59:38 +0900

    problem31

---
+++ problem31/beak31.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class beak31{
+         public static void main(String[] args){
+                 Scanner scan=new Scanner(System.in);
+                 int a=-1,b=-1;
+                 while(a!=0&&b!=0){
+                        a=scan.nextInt();
+                        b=scan.nextInt();
+                        System.out.print((a+b)+"\n");
+                 }
+         }
+} 
