80719b5e93254ba38f4c7b78786e21850cf1df5c
mjh000526 <mjh000526@gmail.com>
2019-07-15 23:58:22 +0900

    problem34

---
+++ problem34/beak34.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class beak34{
+        public static void main(String[] args){
+                Scanner scan=new Scanner(System.in);
+                int a=scan.nextInt();
+                int list []=new int[a];
+                for(int i=0;i<a;i++)
+                {
+                     list[i]=scan.nextInt();       
+                }
+                int(
+        }
+}
