ae4c55ff9b706d683cdc67e3740741c77d2f5902
mjh000526 <mjh000526@gmail.com>
2019-07-20 15:22:06 +0900

    problem43

---
+++ problem15596/beak15596.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class beak15596{
+                 long sum(int[] a){
+                      long sum=0;
+                      for(int i=0;i<a.length; i++){
+                            sum+=a[i];
+                      }
+                      return sum;
+                 }
+}
